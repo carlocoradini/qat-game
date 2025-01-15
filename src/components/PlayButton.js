0d8ec313df31fff00989fccfd940c1d4f57d878f
carlocoradini <carlo.schirmer.110@ufrn.edu.br>
2025-01-15 15:34:00 -0300

    Deploy build

--- src/components/PlayButton.js
+++
@@ -1,9 +0,0 @@
-import "./PlayButton.css"
-
-export default function PlayButton({onClick}) {
-    return (
-        <div className="text-center my-5">
-            <button className="btn btn-play" onClick={onClick}>Play</button>
-        </div>
-    )
-}
--- src/services/wordApi.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const BASE_URL = "https://api.dictionaryapi.dev/api/v2/entries/en"
-
-export const validateWord = async (word) => {
-    const response = await axios.get(`${BASE_URL}/${word}`)
-    return response.data;
-}
